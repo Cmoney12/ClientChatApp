8f9ed35abb54c38f1ba4237b77545d8c96e6f5db
coreylovette <coreylovette@gmail.com>
2021-02-17 10:10:14 -0600

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,70 @@
+//
+// Created by coreylovette on 2/17/21.
+//
+#include <QFormLayout>
+#include <QTcpSocket>
+#include <QWidget>
+#include <QHostAddress>
+#include <QString>
+#include <QThread>
+#include <QStandardItemModel>
+#include <QStringLiteral>
+
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+        : QMainWindow(parent)
+        , socket(new QTcpSocket)
+{
+    auto *widget = new QWidget;
+    auto *layout = new QFormLayout;
+    widget->setLayout(layout);
+    connect_button = new QPushButton("connect");
+    message_view = new QTextEdit;
+    send_button = new QPushButton("Send");
+    message_line = new QLineEdit;
+    layout->addWidget(connect_button);
+    layout->addWidget(message_view);
+    layout->addRow(send_button, message_line);
+    setCentralWidget(widget);
+    connect(connect_button, &QPushButton::clicked, this, &MainWindow::connection);
+    connect(send_button, &QPushButton::clicked, this, &MainWindow::sendMessage);
+    connect(message_line, &QLineEdit::returnPressed, this, &MainWindow::sendMessage);
+    connect(socket, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
+}
+
+
+
+void MainWindow::connection() {
+
+    socket->connectToHost("127.0.0.1", 1234);
+}
+
+void MainWindow::sendMessage() {
+    QString message = message_line->text() + "\n";
+    if(message.isEmpty()) {
+        return;
+    } else {
+        QDataStream clientStream(socket);
+        clientStream << message;
+        message_line->clear();
+    }
+}
+
+void MainWindow::onReadyRead()
+{
+    // We'll loop over every (complete) line of text that the server has sent us:
+    while(socket->canReadLine())
+    {
+        // Here's the line the of text the server sent us (we use UTF-8 so
+        // that non-English speakers can chat in their native language)
+        QString line = QString::fromUtf8(socket->readLine()).trimmed();
+        message_view->append(line);
+    }
+}
+
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
